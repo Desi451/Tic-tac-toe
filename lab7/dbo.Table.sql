aee3ff3fc63cc9599a0f23a6fb563ac34acd7264
Desi451 <mdesortes00@gmail.com>
2023-04-21 23:23:57 +0200

    Project fix

--- lab7/dbo.Table.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[Id] INT NOT NULL , 
-    [gameID] NCHAR(50) NOT NULL, 
-    [gameScore] NCHAR(50) NULL, 
-    [timeGame] NCHAR(50) NULL, 
-    PRIMARY KEY ([gameID])
-)
